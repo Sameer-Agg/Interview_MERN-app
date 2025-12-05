98de513dcb45d4bea4ed995a5c50810b64dcd2e5
Sameer <sameeraggarwal2095@gmail.com>
2025-12-05 15:53:36 +0530

    done

--- server/src/index.js
+++ server/src/index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const app = express();
 const cors = require('cors');
+const path = require('path');
 const ENV = require('./lib/env.js');
 app.use(cors());
 
@@ -12,10 +13,30 @@ app.use(express.urlencoded({ extended: true }));
 
 
 
-const port = process.env.PORT || 3000
+
 app.get('/health',(req,res)=>{
     res.status(201).json({message:'success from api'})
 })
+
+app.get('/books',(req,res)=>{
+    res.status(201).json({message:'books endpoint'})
+})
+
+if(ENV.NODE_ENV === "production"){
+    app.use(express.static(path.join(__dirname,"../../client/dist")));
+    app.get('/{*any}',(req,res)=>{
+        res.sendFile(path.join(__dirname,"../../client/dist/index.html"))
+    })
+}
+
+// if (ENV.NODE_ENV === "production") {
+//   app.use(express.static(path.join(__dirname, "../../client/dist")));
+
+//   app.use((req, res) => {
+//     res.sendFile(path.join(__dirname, "../../client/dist/index.html"));
+//   });
+// }
+
 
 app.listen(ENV.PORT,()=>{
     console.log("server started at ",ENV.PORT);
